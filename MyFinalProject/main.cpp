3feb9b196c7bb081dd7f2e2fada9e5d4b466fa77
jewel <jewel@EHIGHT_OMNI>
2026-05-05 17:00:22 -0500

    I changed a few things

--- MyFinalProject/main.cpp
+++ MyFinalProject/main.cpp
@@ -31,13 +31,24 @@ vector<string> splitRecord_withStringStream(const string& record, char delimiter
 	return fields;
 }
 
+string toLower(string words)
+{
+	for (int i = 0; i < words.length(); i++)
+	{
+		words[i] = tolower(words[i]);
+	}
+	return words;
+}
+
 void searchWord(vector<Verse>& verses, string keyWord) //search from algorithm library
 {
 	bool wordInVerse = false;
+	keyWord = toLower(keyWord);
 	for (const auto& verse : verses)
 	{
-		auto result = search(verse.text.begin(), verse.text.end(), keyWord.begin(), keyWord.end());
-		if (result != verse.text.end())
+		string lowerText = toLower(verse.text);
+		auto result = search(lowerText.begin(), lowerText.end(), keyWord.begin(), keyWord.end());
+		if (result != lowerText.end())
 		{
 			cout << verse.book << " " << verse.chapter << " : " << verse.verse << " - " << verse.text << endl;
 			wordInVerse = true;
@@ -45,7 +56,7 @@ void searchWord(vector<Verse>& verses, string keyWord) //search from algorithm l
 	}
 	if (wordInVerse == false)
 	{
-		cout << "This word is not in the Bible." << endl;
+		cout << "'" << keyWord << "' is not in this version of the Bible.\nMaybe check the spelling and try again?\n";
 	}
 }
 
@@ -70,9 +81,10 @@ void searchForVerse(vector<Verse>& verses, string searchBook, int chaptNum, int 
 void searchForBook(vector<Verse>& verses, string searchBook)
 {
 	bool bookFound = false;
+	searchBook = toLower(searchBook);
 	for (int i = 0; i < verses.size(); i++)
 	{
-		if (verses[i].book == searchBook)
+		if (toLower(verses[i].book) == searchBook)
 		{
 			cout << verses[i].chapter << " : " << verses[i].verse << " - " << verses[i].text << endl;
 			bookFound = true;
@@ -90,6 +102,29 @@ void randomVerse(vector<Verse>& verses)
 	cout << verses[i].book << " " << verses[i].chapter << " : " << verses[i].verse << " - " << verses[i].text << endl;
 }
 
+void randomVerseFromBook(vector<Verse>& verses, string searchBook)
+{
+	vector<Verse> bookVerses;
+	searchBook = toLower(searchBook);
+	for (int i = 0; i < verses.size(); i++)
+	{
+		if (toLower(verses[i].book) == searchBook)
+		{
+			bookVerses.push_back(verses[i]);
+		}
+	}
+
+	if (bookVerses.size() > 0)
+	{
+		int i = rand() % bookVerses.size();
+		cout << bookVerses[i].book << " " << bookVerses[i].chapter << " : " << bookVerses[i].verse << " - " << bookVerses[i].text << endl;
+	}
+	else
+	{
+		cout << "This is not a book in the Bible." << endl;
+	}
+}
+
 map<string, int> countVerses(vector<Verse>&verses) //counts how many verses are in each book
 {
 	map<string, int> mappyMcMapster;
@@ -98,6 +133,61 @@ map<string, int> countVerses(vector<Verse>&verses) //counts how many verses are 
 		mappyMcMapster[verses[i].book]++;
 	}
 	return mappyMcMapster;
+} 
+
+void longestBook(map<string, int>& bookAmount)
+{
+	string longestBook;
+	int maxLength = 0;
+	for (auto book : bookAmount) //for eveery book in the book map
+	{
+		string bookName = book.first; //first = first thing in the map
+		int verseCount = book.second; //second = second thing in the map
+		if (verseCount > maxLength)
+		{
+			longestBook = bookName;
+			maxLength = verseCount;
+		}
+	}
+	cout << "Longest Book: " << longestBook << " has " << maxLength << " verses" << endl;
+}
+
+void shortestBook(map<string, int>& bookAmount)
+{
+	string shortestBook;
+	int minLength = 100000;
+	for (auto book : bookAmount) //for eveery book in the book map
+	{
+		string bookName = book.first; //first = first thing in the map
+		int verseCount = book.second; //second = second thing in the map
+		if (verseCount < minLength)
+		{
+			shortestBook = bookName;
+			minLength = verseCount;
+		}
+	}
+	cout << "Shortest Book: " << shortestBook << " has " << minLength << " verses" << endl;
+}
+
+void averageVerseLength(vector<Verse>& verses)
+{
+	int totalWords = 0;
+	for (int i = 1; i < verses.size(); i++)
+	{
+		stringstream stringStream(verses[i].text);
+		string word;
+		int wordsInVerse = 0; //haha not inverse "in verse"
+		while (getline(stringStream, word, ' '))
+		{
+			if (word.size() > 0)
+			{
+				wordsInVerse++;
+			}
+		}
+		totalWords += wordsInVerse;
+	}
+	int averageVerseLength = totalWords / verses.size();
+	cout << "Average Verse Length: " << averageVerseLength << " words" << endl;
 }
 
 void longestVerse(vector<Verse>& verses)
@@ -110,7 +200,7 @@ void longestVerse(vector<Verse>& verses)
 			maxLength = i;
 		}
 	}
-	cout << verses[maxLength].text << endl;
+	cout << "Longest Verse: " << verses[maxLength].text << endl;
 }
 
 void shortestVerse(vector<Verse>& verses)
@@ -123,7 +213,19 @@ void shortestVerse(vector<Verse>& verses)
 			minLength = i;
 		}
 	}
-	cout << verses[minLength].text << endl;
+	cout << "Shortest Verse: " << verses[minLength].text << endl;
+}
+
+void bibleStatistics(vector<Verse>& verses)
+{
+	cout << "Total Verses: " << verses.size() << endl;
+	map<string, int> bookAmount = countVerses(verses);
+	cout << "Total Books: " << bookAmount.size() << endl;
+	longestBook(bookAmount);
+	shortestBook(bookAmount);
+	averageVerseLength(verses);
+	longestVerse(verses);
+	shortestVerse(verses);
 }
 
 int main()
@@ -160,17 +262,18 @@ int main()
 	string searchBook;
 	map<string, int> bookCounts = countVerses(verses);
 	srand(static_cast<unsigned int>(time(0)));
-	cout << "Hello :) This program is intended to allow you to search through Bible Verses from the KJV Version.\n\n";
+	cout << "Hello :) This program is intended to allow you to search through Bible Verses from the KJV Version.\nPlease take into account that the spelling is different in the KJV version.\n\n";
 	do
 	{
-		cout << "Please choose how you would like to search: \n0. Exit\n1. Search by Keyword\n2. Search for Verse\n3. Search for Book\n4. Random Verse\n5. Search for Number of Verses in a Book\n6. Longest Verse\n7. Shortest Verse" << endl;
+		cout << "Please choose how you would like to search: \n0. Exit\n1. Search by Keyword\n2. Search for Verse\n3. Search for Book\n4. Random Verse\n5. Random Verse from Book \n6. Search for Number of Verses in a Book\n7. Bible Statistics" << endl;
 		cin >> option;
 
 		switch (option)
 		{
 		case 1: //Search for Keyword
-			cout << "Please enter the word you would like to search for: ";
-			cin >> keyWord;
+			cout << "Please enter the word or phrase you would like to search for: ";
+			cin.ignore(1000, '\n');
+			getline(cin, keyWord);
 			cout << "Entering Keyword search..." << endl;
 			searchWord(verses, keyWord);
 			break;
@@ -185,7 +288,7 @@ int main()
 			searchForVerse(verses, searchBook, chaptNum, verseNum);
 			break;
 		case 3: //Search for Book
-			cout << "Please enter the book you would like to search for (Please Capitalize): ";
+			cout << "Please enter the book you would like to search for: ";
 			cin >> searchBook;
 			cout << "Searching for Book..." << endl;
 			searchForBook(verses, searchBook);
@@ -194,19 +297,22 @@ int main()
 			cout << "Generating a random verse..." << endl;
 			randomVerse(verses);
 			break;
-		case 5: // Number of Verses in book
+		case 5: // Random Verse in Book
+			cout << "Please enter the book you would like a random verse from: ";
+			cin >> searchBook;
+			cout << "Generating a random verse..." << endl;
+			randomVerseFromBook(verses, searchBook);
+			break;
+		case 6: // Number of Verses in book
 			cout << "Please enter the book you would like to search the number of verses for: ";
 			cin >> searchBook;
 			cout << "Counting number of verses in book..." << endl;
 			cout << bookCounts[searchBook] << "\n\n";
 			break;
-		case 6: // Longest Book
-			cout << "Searching for the Longest Verse..." << endl;
-			longestVerse(verses);
-			break;
-		case 7: // Shortest Book
-			cout << "Searching for the Shortest Verse..." << endl;
-			shortestVerse(verses);
+		case 7: // Bible Statistics
+			cout << "Outputting Bible Statistics..." << endl;
+			bibleStatistics(verses);
+			cout << "\n";
 			break;
 		case 0: //Exit
 			cout << "Exiting program...\n";
